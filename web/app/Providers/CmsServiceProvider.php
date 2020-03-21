471ac2768043af33a770a23a3ddd8c7e2d831b2f
Manuel Christlieb <manuel@christlieb.eu>
2020-03-21 11:18:06 +0100

    fix(): Use new FrontendRouter

--- web/app/Providers/CmsServiceProvider.php
+++ web/app/Providers/CmsServiceProvider.php
@@ -2,12 +2,12 @@
 
 namespace App\Providers;
 
-use Bambamboole\LaravelCms\Services\CmsRouter;
+use Bambamboole\LaravelCms\Routing\FrontendRouter;
 use Illuminate\Support\ServiceProvider;
 
 class CmsServiceProvider extends ServiceProvider
 {
-    public function boot(CmsRouter $router)
+    public function boot(FrontendRouter $router)
     {
         $router->registerPagesRoutes();
         $router->registerPostsRoutes();
