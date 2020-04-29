62b84c88c4eacd727128b61818478f2358ad6028
Manuel Christlieb <manuel@christlieb.eu>
2020-04-29 21:01:57 +0200

    add example on how to extend the admin

--- web/app/Providers/CmsServiceProvider.php
+++ web/app/Providers/CmsServiceProvider.php
@@ -2,14 +2,22 @@
 
 namespace App\Providers;
 
+use Oscer\Cms\Backend\ScriptHandler;
+use Oscer\Cms\Backend\Sidebar\Sidebar;
+use Oscer\Cms\Backend\Sidebar\SidebarItem;
 use Oscer\Cms\Frontend\Routing\FrontendRouter;
 use Illuminate\Support\ServiceProvider;
 
 class CmsServiceProvider extends ServiceProvider
 {
-    public function boot(FrontendRouter $router)
+    public function boot(FrontendRouter $router, Sidebar $sidebar, ScriptHandler $handler)
     {
         $router->registerPagesRoutes();
         $router->registerPostsRoutes();
+
+        $handler->addScript('/js/app.js');
+
+        $sidebar->addItem(new SidebarItem('folder', 'foobar', 'foo.bar', 'users.view'));
+
     }
 }
